367c192fae49ef68060680d29cfb99c2b1ce68bf
Lucas-yago <41453379+Lucas-yago@users.noreply.github.com>
2022-02-02 16:17:39 -0300

    adiciona cliente  e projeto react

---
+++ client/src/App.js
@@ -0,0 +1,15 @@
+import  Inicio from "./pages/cliente/painel";
+
+function App() {
+  return (
+    <div className="App">
+      {/* <header className="App-header">
+        <h1>ERRO: 408</h1>
+        <h3>Comeu o cu do curioso!</h3>
+      </header> */}
+      <Inicio/>
+    </div>
+  );
+}
+
+export default App;
