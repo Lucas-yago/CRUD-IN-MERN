52679ae55b8b7e1587d160e78b572bedd53fedf1
Lucas-yago <41453379+Lucas-yago@users.noreply.github.com>
2022-02-10 14:42:00 -0300

    adiciona autenticaçao front-end

--- client/src/pages/admin/login/index.jsx
+++ client/src/pages/admin/login/index.jsx
@@ -1,4 +1,4 @@
-import {useState} from 'react';
+import { useState } from 'react';
 import Avatar from '@mui/material/Avatar';
 import Button from '@mui/material/Button';
 import CssBaseline from '@mui/material/CssBaseline';
@@ -9,6 +9,8 @@ import Typography from '@mui/material/Typography';
 import Container from '@mui/material/Container';
 import { createTheme, ThemeProvider } from '@mui/material/styles';
 import { Copyright } from '../../../components/footer-admin';
+import api from '../../../services/api';
+import { login, setIdUser, setNameUser } from '../../../services/auth'
 
 
 const theme = createTheme();
@@ -18,7 +20,21 @@ export const Login = () => {
     const [password, setPassword] = useState('');
 
     const handleSubmit = async () => {
-        alert("autenticar: "+email);
+        await api.post('/api/v1/users/login', { email, password })
+            .then(res => {
+                if (res.status === 200) {
+                    if (res.data.status === 1) {
+                        login(res.data.token);
+                        setIdUser(res.data.id_user);
+                        setNameUser(res.data.user_name);
+                        window.location.href = "/admin"
+                    } else if (res.data.status === 2) {
+                        alert("Attention! " + res.data.error);
+                    }
+                } else {
+                    alert("Internal sever error");
+                }
+            })
     };
 
     return (
@@ -39,39 +55,39 @@ export const Login = () => {
                     <Typography component="h1" variant="h5">
                         Sign in
                     </Typography>
-                    <Box   noValidate sx={{ mt: 1 }}>
-                        <TextField
-                            margin="normal"
-                            required
-                            fullWidth
-                            id="email"
-                            label="Email Address"
-                            name="email"
-                            autoFocus
-                            value={email}
-                            onChange={ e => setEmail(e.target.value)}
-                        />
-                        <TextField
-                            margin="normal"
-                            required
-                            fullWidth
-                            name="password"
-                            label="Password"
-                            type="password"
-                            id="password"
-                            value={password}
-                            onChange={e => setPassword(e.target.value)}
-                        />
-                        <Button
-                            // type="submit"
-                            fullWidth
-                            variant="contained"
-                            sx={{ mt: 3, mb: 2 }}
-                            onClick={handleSubmit}
-                        >
-                            Sign In
-                        </Button>
-                    </Box>
+                    {/* <Box sx={{ mt: 1 }}> */}
+                    <TextField
+                        margin="normal"
+                        required
+                        fullWidth
+                        id="email"
+                        label="Email Address"
+                        name="email"
+                        autoFocus
+                        value={email}
+                        onChange={e => setEmail(e.target.value)}
+                    />
+                    <TextField
+                        margin="normal"
+                        required
+                        fullWidth
+                        name="password"
+                        label="Password"
+                        type="password"
+                        id="password"
+                        value={password}
+                        onChange={e => setPassword(e.target.value)}
+                    />
+                    <Button
+                        // type="submit"
+                        fullWidth
+                        variant="contained"
+                        sx={{ mt: 3, mb: 2 }}
+                        onClick={handleSubmit}
+                    >
+                        Sign In
+                    </Button>
+                    {/* </Box> */}
                 </Box>
                 <Copyright />
             </Container>
---
+++ client/src/services/auth.js
@@ -0,0 +1,16 @@
+export const TOKEN_KEY = '&app-token';
+export const ID_USER = '&id-user';
+export const NAME_USER = '&name-user';
+export const TYPE_USER = '&type-user';
+
+export const login = token => localStorage.setItem(TOKEN_KEY,token);
+export const logout = () => localStorage.clear();
+
+export const setIdUser = id => localStorage.setItem(ID_USER,id);
+export const getIdUser = () => localStorage.getItem(ID_USER)
+
+export const setNameUser = name => localStorage.setItem(NAME_USER,name);
+export const getNameUser = () => localStorage.getItem(NAME_USER)
+
+export const setTypeUser = type => localStorage.setItem(TYPE_USER,type);
+export const getTypeUser = () => localStorage.getItem(TYPE_USER);
